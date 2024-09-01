84521671b1d95be875f061f1453a179f372a90ad
kunitate.katagiri <kt.katagiri@gmail.com>
2024-09-01 15:36:20 +0900

    Fix

---
+++ python/lib/slack_webhook.py
@@ -0,0 +1,9 @@
+import requests
+
+class SlackWebhook:
+    def __init__(self, webhook_url):
+        self.webhook_url = webhook_url
+
+    def send_message(self, message):
+        # Code to send message to Slack
+        pass
--- python/main.py
+++ python/main.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
-# Your code goes here
+import sys
+import os
+
+# libディレクトリをモジュール検索パスに追加
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+
+from lib.slack_webhook import SlackWebhook
+
+def main():
+    # Your code goes here
+    pass
+
+if __name__ == "__main__":
+    main()
